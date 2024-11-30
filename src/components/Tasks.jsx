731aaac12aa27bd08a14a51efe7fa2cd23660f33
Ethan Cornwill <ethancornwill@outlook.com>
2024-11-30 11:27:03 +1100

    style: Refactor Tasks component for improved readability and consistency

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -1,37 +1,40 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { useLocalStorage } from 'react-use';
+import React, { useContext } from 'react'
+import { Link } from 'react-router-dom'
+import { useLocalStorage } from 'react-use'
+import { useTasks } from '../contexts/TaskContext'
 
 const Tasks = () => {
-    const [tasks, setTasks] = useLocalStorage('tasks', []);
+  const [tasks, setTasks] = useLocalStorage('tasks', [])
 
-    const deleteTask = (id) => {
-        const updatedTasks = tasks.filter((task) => task.id != id);
-        setTasks(updatedTasks);
-    };
+  const deleteTask = (id) => {
+    const updatedTasks = tasks.filter((task) => task.id !== id)
+    setTasks(updatedTasks)
+  }
 
-    return (
-        <div>
-            <h2>Tasks</h2>
-            {
-                tasks.length === 0 ? (
+  return (
+    <div>
+      <h2>Tasks</h2>
+      {
+                tasks.length === 0
+                  ? (
                     <p>No tasks found. <Link to='/add-task'>Add your first task!</Link></p>
-                ) : (
+                    )
+                  : (
                     <ul>
-                        {tasks.map((task) =>
-                            (<li key={task.id}>
-                                <strong>{task.title}</strong>: {task.description}
-                                <div>
-                                    <Link to={`/edit-task/${task.id}`}>Edit</Link> | {' '}
-                                    <button onClick={() => deleteTask(task.id)}>Delete</button>
-                                </div>
-                            </li>)
-                        )}
+                      {tasks.map((task) =>
+                        (<li key={task.id}>
+                          <strong>{task.title}</strong>: {task.description}
+                          <div>
+                            <Link to={`/edit-task/${task.id}`}>Edit</Link> | {' '}
+                            <button onClick={() => deleteTask(task.id)}>Delete</button>
+                          </div>
+                        </li>)
+                      )}
                     </ul>
-                )
+                    )
             }
-        </div>
-    );
+    </div>
+  )
 }
 
-export default Tasks;
+export default Tasks
